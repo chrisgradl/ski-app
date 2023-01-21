f833308b7b7f14922c1449cdbc3d2a7f1d403393
cgradl <christian.gradl@icloud.com>
2023-01-21 16:06:58 +0100

    add deeplink

--- app/src/main/java/at/cgradl/skiapp/MainActivity.kt
+++ app/src/main/java/at/cgradl/skiapp/MainActivity.kt
@@ -2,7 +2,10 @@ package at.cgradl.skiapp
 
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -26,20 +29,62 @@ import androidx.compose.ui.unit.dp
 import at.cgradl.skiapp.model.PersonRanking
 import at.cgradl.skiapp.ui.theme.SkiAppTheme
 import coil.compose.rememberAsyncImagePainter
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
+
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         val jsonResults: String =
             applicationContext.assets.open("ranking.json").bufferedReader().use { it.readText() }
 
         val listType: Type = object : TypeToken<List<PersonRanking>>() {}.type
         val rankings: List<PersonRanking> = Gson().fromJson(jsonResults, listType);
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("TAG", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            val token = task.result
+
+            // Log and toast
+            val msg = "Token is $token"
+            Log.d("TAG", msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
+
+        FirebaseDynamicLinks.getInstance()
+            .getDynamicLink(intent)
+            .addOnSuccessListener(
+                this
+            ) { pendingDynamicLinkData ->
+                // Get deep link from result (may be null if no link is found)
+                var deepLink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    deepLink = pendingDynamicLinkData.link
+                }
+                val personId = deepLink?.getQueryParameter("PersonId")
+                personId?.let {
+                    var ranking = rankings.find { it.PersonId.toString() == personId }
+                    ranking?.let {
+                        this.openDetailScreen(ranking);
+                    }
+                }
+
+            }
+            .addOnFailureListener(
+                this
+            ) { e -> Log.w("TAG", "getDynamicLink:onFailure", e) }
 
         setContent {
             SkiAppTheme {
